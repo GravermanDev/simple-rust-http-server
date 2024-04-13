16c3b0c3283c9949ed2cecf704a03de56c31454c
GravermanDev <swiatmetalu@gmail.com>
2024-04-13 15:13:52 +0200

    parsing http requests

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,17 @@ fn send_ok(mut stream: TcpStream) {
     println!("Sent an OK response");
 }
 
+fn send_path_in_body(mut stream: TcpStream, http_request: Vec<String>) {
+    if let Some(request_line) = http_request.get(0) {
+        let parts: Vec<&str> = request_line.split_whitespace().collect();
+        if parts.len() >= 3 && parts[0] == "GET" {
+            let path = parts[1];
+            let path_len = path.len();
+            stream.write_all(format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Lenght: {path_len}\r\n\r\n{path}\r\n\r\n").as_bytes()).unwrap();
+        }
+    }
+}
+
 fn send_not_found(mut stream: TcpStream) {
     stream.write_all("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes()).unwrap();
     println!("Send a Not Found response")
@@ -13,21 +24,13 @@ fn send_not_found(mut stream: TcpStream) {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
+    let http_request: Vec<String> = buf_reader
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
 
-    if let Some(request_line) = http_request.get(0) {
-        let parts: Vec<&str> = request_line.split_whitespace().collect();
-        if parts.len() >= 3 && parts[0] == "GET" {
-            match parts[1] {
-                "/" => send_ok(stream),
-                _ => send_not_found(stream),
-            }
-        }
-    }
+    send_path_in_body(stream, http_request);
 }
 
 fn main() {
